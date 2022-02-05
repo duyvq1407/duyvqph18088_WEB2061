be3190219d49d333fbee311b04e51be4a33a81d4
Vũ Quang Duy <vduy2350@gmail.com>
2022-02-05 23:31:13 +0700

    update

--- src/components/promo-bar.js
+++ src/components/promo-bar.js
@@ -1,9 +1,11 @@
 const PromoBar = {
     render() {
         return /* html */`
-                <a href="#" class="text-[10px] py-[10px] block font-normal">
-                    <span class="text-white leading-[10px]">SHIP HÀNG TOÀN QUỐC - ĐỔI TRẢ TRONG 07 NGÀY - ĐẢM BẢO CHẤT LƯỢNG</span>
+            <div  class="bg-[#4267b2] w-[100%] leading-[10px] duration-300 text-center cursor-pointer z-170 overflow-hidden">
+                <a href="#" class="text-white text-[10px] py-[10px] block">
+                    <span>SHIP HÀNG TOÀN QUỐC - ĐỔI TRẢ TRONG 07 NGÀY - ĐẢM BẢO CHẤT LƯỢNG</span>
                 </a>
+            </div>
         `;
     },
 };
