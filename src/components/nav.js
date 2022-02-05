8cf93f3635375a0aac98628cb29b310e35eb47e9
Vũ Quang Duy <vduy2350@gmail.com>
2022-02-05 23:35:16 +0700

    update navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,7 @@ const NavBar = {
     render() {
         return /* html */`
             <div  class="bg-[#4267b2] w-[100%] leading-[10px] duration-300 text-center cursor-pointer z-170 overflow-hidden">
-                <p href="#" class="text-white text-[xx-small] py-[10px] block">
+                <p class="text-white text-[10px] py-[10px] block">
                     SHIP HÀNG TOÀN QUỐC - ĐỔI TRẢ TRONG 07 NGÀY - ĐẢM BẢO CHẤT LƯỢNG
                 </p>
             </div>
