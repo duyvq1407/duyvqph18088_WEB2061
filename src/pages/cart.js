f4649f34c462899d014eebaaa43f06315a175f73
Vũ Quang Duy <vduy2350@gmail.com>
2022-02-25 07:05:52 +0700

    update

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -2,7 +2,7 @@ import Header from "../components/header";
 
 const CartPage = {
     async render() {
-        // const cart = JSON.parse(localStorage.getItem("cart"));
+        const cart = JSON.parse(localStorage.getItem("cart"));
         return /* html */`
             ${await Header.render()}
             <div class="overflow-hidden pt-[130px]" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
@@ -28,55 +28,33 @@ const CartPage = {
                             <div class="mt-8">
                             <div class="flow-root">
                                 <ul role="list" class="-my-6 divide-y divide-gray-200">
-                                <li class="flex py-6">
-                                    <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                    <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." class="h-full w-full object-cover object-center">
-                                    </div>
-                
-                                    <div class="ml-4 flex flex-1 flex-col">
-                                    <div>
-                                        <div class="flex justify-between text-base font-medium text-gray-900">
-                                        <h3>
-                                            <a href="#"> Throwback Hip Bag </a>
-                                        </h3>
-                                        <p class="ml-4">$90.00</p>
+                                ${cart.map((cart1) => `
+                                    <li class="flex py-6">
+                                        <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                        <img src="${cart1.image}" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." class="h-full w-full object-cover object-center">
                                         </div>
-                                        <p class="mt-1 text-sm text-gray-500">Salmon</p>
-                                    </div>
-                                    <div class="flex flex-1 items-end justify-between text-sm">
-                                        <p class="text-gray-500">Qty 1</p>
-                
-                                        <div class="flex">
-                                        <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                    
+                                        <div class="ml-4 flex flex-1 flex-col">
+                                        <div>
+                                            <div class="flex justify-between text-base font-medium text-gray-900">
+                                            <h3>
+                                                <a href="#"> ${cart1.name} </a>
+                                            </h3>
+                                            <p class="ml-4">${new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(cart1.price * cart1.quantity)}</p>
+                                            </div>
+                                            <p class="mt-1 text-sm text-gray-500">Salmon</p>
                                         </div>
-                                    </div>
-                                    </div>
-                                </li>
-                
-                                <li class="flex py-6">
-                                    <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                    <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg" alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch." class="h-full w-full object-cover object-center">
-                                    </div>
-                
-                                    <div class="ml-4 flex flex-1 flex-col">
-                                    <div>
-                                        <div class="flex justify-between text-base font-medium text-gray-900">
-                                        <h3>
-                                            <a href="#"> Medium Stuff Satchel </a>
-                                        </h3>
-                                        <p class="ml-4">$32.00</p>
+                                        <div class="flex flex-1 items-end justify-between text-sm">
+                                            <p class="text-gray-500">Quantity: ${cart1.quantity}</p>
+                    
+                                            <div class="flex">
+                                            <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                            </div>
                                         </div>
-                                        <p class="mt-1 text-sm text-gray-500">Blue</p>
-                                    </div>
-                                    <div class="flex flex-1 items-end justify-between text-sm">
-                                        <p class="text-gray-500">Qty 1</p>
-                
-                                        <div class="flex">
-                                        <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
                                         </div>
-                                    </div>
-                                    </div>
-                                </li>
+                                    </li>
+
+                                `)}
                                 </ul>
                             </div>
                             </div>
