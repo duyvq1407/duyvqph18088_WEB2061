79cd2de23ca01faac2205b81545f3db0b2e93f5b
Vũ Quang Duy <vduy2350@gmail.com>
2022-02-25 08:07:20 +0700

    update

--- utils/index.js
+++ utils/index.js
@@ -9,8 +9,3 @@ export const reRender = async (dom, component, id = "") => {
     document.querySelector(dom).innerHTML = await component.render(id);
     if (component.afterRender) component.afterRender(id);
 };
-
-export const $ = (element) => {
-    const selectors = document.querySelectorAll(element);
-    return selectors.length === 1 ? selectors[0] : selectors;
-};
